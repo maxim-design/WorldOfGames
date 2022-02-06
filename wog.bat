9ce2827749e7129594f9fe70bfbf010820ee3bb5
maxim-design <97593405+maxim-design@users.noreply.github.com>
2022-02-06 21:11:56 +0200

    added auto brawser open for leadrboard to bat/sh files.

--- wog.bat
+++ wog.bat
@@ -1,5 +1,6 @@
 echo off
 python initialize.py
 start "Flask" /min cmd /k "python Leaderboard.py"
+start http://127.0.0.1:30000/
 python MainGame.py
 for /f "tokens=2 delims=," %%a in ('tasklist /fi "imagename eq cmd.exe" /v /fo:csv /nh ^| findstr /r /c:".*Flask[^,]*$" ') do taskkill /pid %%a
--- wog.sh
+++ wog.sh
@@ -1,4 +1,5 @@
 python3 initialize.py
 python3 Leaderboard.py &>/dev/null &
+open http://127.0.0.1:30000/
 python3 MainGame.py
 kill -9 $PPID
